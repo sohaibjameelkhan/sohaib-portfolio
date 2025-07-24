1be009d895441e23f687536537a971bcf0b2d68c
sohaib37104 <127841537+sohaib37104@users.noreply.github.com>
2025-07-24 17:24:10 +0500

    intial branch commit

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         ]
       : []),
   ],
-  base: '/NotepadAI-Portfolio/',
+  base: '/notepadai-portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
