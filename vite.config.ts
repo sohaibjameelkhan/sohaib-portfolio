93415e95172a43cdef785c82b70cecb9a82ce9c8
sohaib37104 <127841537+sohaib37104@users.noreply.github.com>
2025-07-24 16:17:46 +0500

    issues fixed

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base: '/NotepadAI-Portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
